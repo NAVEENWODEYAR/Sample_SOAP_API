b6e086a285911982542e9508d4a45b8df32f9a96
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-09 20:15:30 +0530

    @Override

--- src/main/java/com/people/PeopleImpl.java
+++ src/main/java/com/people/PeopleImpl.java
@@ -11,17 +11,14 @@ public class PeopleImpl implements People {
     private String name=null;
     private int age=0;
    
-    @Override
     public void setName(String name) {
 	this.name=name;
     }
 
-    @Override
     public String getName() {
 	return name;
     }
 
-    @Override
     public int getAge() {
 	return age;
     }
